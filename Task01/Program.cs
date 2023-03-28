445746d74f598773d36757296c8008f56dffd820
Aleksandra <sasha-skaliarka@mail.ru>
2023-03-28 17:29:58 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.WriteLine("введите первое целое число");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе целое число");
+int second_number = Convert.ToInt32(Console.ReadLine());
+if (first_number == second_number * second_number)
+{
+Console.WriteLine($"число {first_number} является квадратом числа {second_number}");
+}
+else 
+{
+Console.WriteLine($"число {first_number} не является квадратом числа {first_number}");
+}
