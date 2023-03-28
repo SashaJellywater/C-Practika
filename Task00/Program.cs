aa984260011493d529186db3f83e5782554c4e87
Aleksandra <sasha-skaliarka@mail.ru>
2023-03-28 16:53:44 +0300

    Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿//программа принимает число и выдаёт его квадрат
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int square_number = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square_number}");
