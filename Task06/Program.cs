6dabe21e6ea4fa3ec0732c9c8150e2ae43a0a983
Aleksandra <sasha-skaliarka@mail.ru>
2023-03-28 19:30:08 +0300

    Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+Console.WriteLine("введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number %2 == 0)
+{
+    Console.WriteLine($" Ура! число {number} чётное");
+}
+else
+{
+    Console.WriteLine($" О нет! число {number} нечётное");
+}
