5b9bcaf5fb826a250741b7c5a3c71b7ba74a25c7
Aleksandra <sasha-skaliarka@mail.ru>
2023-03-28 17:55:58 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,35 @@
+﻿//программа определяет день недели по введенному числу
+Console.WriteLine("введите число");
+string day = Console.ReadLine();
+if (day == "1")
+{
+    Console.WriteLine("Понедельник...ненавижу понедельники");
+}
+else if (day == "2")
+{
+    Console.WriteLine("Сегодня вторник");
+}
+else if (day =="3")
+{
+    Console.WriteLine("Середина рабочей недели - среда!");
+}
+else if (day == "4")
+{
+    Console.WriteLine("Четверг. Осталось чуть-чуть");
+}
+else if (day == "5")
+{
+    Console.WriteLine("Урра!!! Пятница!");
+}
+else if (day == "6")
+{
+    Console.WriteLine("Суббота - сегодня гуляем!");
+}
+else if (day == "7")
+{
+    Console.WriteLine("Уже воскресенье?! Завтра на работу");
+}
+else
+{
+    Console.WriteLine("Упс! Такого дня недели нет");
+}
